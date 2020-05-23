d4d15dad3e646f7564e6a1e23611ab38d808f29b
Pinefoot <jameslcarpino@gmail.com>
2020-05-23 13:09:20 -0700

    added dependencies, building out file structure

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+// Update with your config settings.
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/medcabinet";
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './database/medcabinet'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds:{
+      directory: './data/seeds'
+    }
+  },
+  
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  production: {
+    client: 'pg',
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migratoins'
+    },
+    seeds: {
+      directory: './data/seed'
+    }
+  }
+
+};
